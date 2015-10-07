b17aecf3dee89e9a3fb0dd2b7ac4868be17a7d80
Mona <monashh_m@hotmail.com>
2015-10-07 07:17:05 +0200

    Creado test testIsBiggerThan en la clase FractionTest

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -5,27 +5,33 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class FractionTest {
 	private Fraction fraction;
-	
+	private Fraction fraction2;
+
 	@Before
-	public void before(){
-		fraction = new Fraction(1,2);
+	public void before() {
+		fraction = new Fraction(1, 2);
 	}
-	
+
 	@Test
 	public void testGetNumerator() {
 		assertEquals(fraction.getNumerator(), 1);
 	}
-	
+
 	@Test
 	public void testGetDenominator() {
 		assertEquals(fraction.getDenominator(), 2);
 	}
-		
+
 	@Test
-	public void testDecimal(){
+	public void testDecimal() {
 		assertEquals(fraction.decimal(), 0.5, 10e-5);
+	}
+
+	@Test
+	public void testIsBiggerThan() {
+		fraction2 = new Fraction(1, 3);
+		assertEquals(true, fraction.isBiggerThan(fraction2));
 	}
 }
